79bf477951d63c68a2edbcd86f77388e43f30321
João Alfredo Pires <joaoalfredopires.prof@gmail.com>
2021-12-24 11:01:28 -0300

    First commit

---
+++ cypress/integration/commerce-flow.spec.js
@@ -0,0 +1,7 @@
+//http://automationpractice.com/index.php
+
+describe("Fluxos do ecommerce", () => {
+    it("Acessar a página inicial", () => {
+          cy.visit("http://automationpractice.com/index.php")  
+    });
+});
